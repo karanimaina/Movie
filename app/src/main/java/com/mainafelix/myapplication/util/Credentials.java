290a042da7caf71a8dbc57e7b6cf076164d388e3
felixmaina <karani.maina2010@gmail.com>
2022-06-30 14:27:58 +0300

    create class Credentials

---
+++ app/src/main/java/com/mainafelix/myapplication/util/Credentials.java
@@ -0,0 +1,6 @@
+package com.mainafelix.myapplication.util;
+
+public class Credentials {
+    public static final String BASE_URL = "https://api.themoviedb.org";
+    public static  final String API_KEY = "07186b458d1053c8f6dc333fb4835471";
+}
