79a6926b740d491da71621869d8c9aac5f3d718a
felixmaina <karani.maina2010@gmail.com>
2022-06-30 15:38:26 +0300

    set view binding

--- app/src/main/java/com/mainafelix/myapplication/MainActivity.java
+++ app/src/main/java/com/mainafelix/myapplication/MainActivity.java
@@ -3,12 +3,29 @@ package com.mainafelix.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.mainafelix.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+Button btn;
+ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater())
+        setContentView(binding.getRoot());
+        binding.btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getRetrofitResponse();
+            }
+        });
+    }
+
+    private void getRetrofitResponse() {
+
     }
 }
